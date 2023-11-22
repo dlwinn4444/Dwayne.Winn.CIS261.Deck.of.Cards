fd9381cf602460ca811e40177349af3627fb3c83
dlwinn4444 <147110735+dlwinn4444@users.noreply.github.com>
2023-11-22 18:03:14 -0500

    working code

--- Dwayne.Winn.CIS261.Deck.of.Cards.py
+++ Dwayne.Winn.CIS261.Deck.of.Cards.py
@@ -9,30 +9,48 @@ class cards:
       
 class Deck:
    def __init__(self):
-      self.ranks = ['ACE','2','3','4','5','6','7','8','9','10','Jack','Gueen','King']
+      self.ranks = ['ACE','2','3','4','5','6','7','8','9','10','Jack','Queen','King']
       self.suits = ['Hearts','Diamonds','Clubs','Spades']
       self.reset()
+      self.Count()
+      
    def reset(self):
-      self.deck = [card(rank, suit) for suit in self.suits for rank in self.ranks]
+      self.deck = [f'{rank} of {suit}' for suit in self.suits for rank in self.ranks]
       
    def shuffle(self):
        random.shuffle(self.deck)
-       
-    def deal(self):
-      if len(self, deck) == 0
+  
+   def Count(self):
+      return len(self.deck)
+  
+   def deal(self):
+      if len(self.deck) == 0:
          return None
-      return.self.deck.pop()
+      return self.deck.pop()
+  
+  
 print("Welcome to the card dealer")
 print()
 print("There is 52 cards in the deck")
 
 deck = Deck()
-
 deck.shuffle()
-num_cards = int(input("how many cards would you like?: "))
-dealt.cards = []
 
-for _ in 
+
+while cards is not None:
+   num_cards = int(input("how many cards would you like?: "))
+   for _ in range(num_cards):
+       card = deck.deal()
+       print(f"Card  is: {card}")
+       count = deck.Count()
+   print(f"Remaining cards in deck: {count}")
+   choice = input("Deal again (y/n)")
+   if choice.lower == "n" :
+      break
+print("No more cards in deck")
+
+
+
 
 
        
